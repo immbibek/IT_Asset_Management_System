b5178820efa61e40b08ad9b29072e4276bae0908
Bibek <137331749+immbibek@users.noreply.github.com>
2025-11-09 00:53:19 +0530

    Wrapping up with AssetProviderContext

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { AssetProvider } from "./context/AssetContext.jsx";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthProvider>
-      <App />
+      <AssetProvider>
+        <App />
+      </AssetProvider>
     </AuthProvider>
   </BrowserRouter>
 );
