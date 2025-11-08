c07b9fd360640d0b6318e54d5ebbbb9beeddf3e4
Bibek <137331749+immbibek@users.noreply.github.com>
2025-11-08 22:21:12 +0530

    Organizing All charts of an Dashboard page in different chart component Now i have Created ASSETBYCATEGORY CHART

---
+++ frontend/src/components/charts/AssetByCategoryChart.jsx
@@ -0,0 +1,25 @@
+import {
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+} from "recharts";
+
+const AssetsByCategoryChart = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Bar dataKey="value" fill="#3b82f6" radius={[8, 8, 0, 0]} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default AssetsByCategoryChart;
