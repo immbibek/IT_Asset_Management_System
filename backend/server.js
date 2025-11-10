3a04eac06bb9db3816bb630cf49b2db2e7469d40
Bibek <137331749+immbibek@users.noreply.github.com>
2025-11-10 18:10:04 +0530

    configured mongodb database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+connectDB();
 
 const PORT = 3000;
 
@@ -9,4 +15,4 @@ app.get("/", (req, res) => {
   res.send("successful");
 });
 
-app.listen(PORT, () => console.log(`app is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
